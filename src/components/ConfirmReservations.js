bbbe1d9a2e9b551a79becc194b3c2062b98fa7bb
Truong Anh Vu <122198260+truonganhvu205@users.noreply.github.com>
2023-11-03 08:27:29 +0700

    Add files via upload

--- src/components/ConfirmReservations.js
+++ src/components/ConfirmReservations.js
@@ -1,14 +1,41 @@
 import React from 'react'
-import { Outlet, Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import confirmReservationsFormStyle from './components.module.css/ConfirmReservations.module.css'
+import lemonLogo from '../images/logo/lemon-logo.png'
+import Header from './Header'
+import Footer from './Footer'
 
 const ConfirmReservations = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div>
-            <h1>The booking has been confirmed</h1>
+            <Header />
 
-            <div>
-                <Link to="/">Home</Link>
+            <div className={confirmReservationsFormStyle.reservationsForm}>
+                <form action="" onSubmit={handleSubmit}>
+                    <div className={confirmReservationsFormStyle.reservationsBox}>
+                        <h1>The Reservation has been confirmed.</h1>
+                        <img src={lemonLogo} alt="logo" />
+                        <h3>Please check your Email for more information!</h3>
+
+                        <div className={confirmReservationsFormStyle.reservationsNote}>
+                            <h3>Note</h3>
+                            <p>The table that you have been reserved will be cancled after 15 minutes if you are late.</p>
+                        </div>
+
+                        <div className={confirmReservationsFormStyle.reservationsLine}></div>
+
+                        <Link to='/'>
+                            <button>Confirm</button>
+                        </Link>
+                    </div>
+                </form>
             </div>
+
+            <Footer />
         </div>
     )
 }
